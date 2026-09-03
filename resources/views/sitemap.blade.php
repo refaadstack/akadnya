1c8d8c1508959cc2f55c892b096ce53c353ba628
refaadstack <refaad16@gmail.com>
2026-09-03 21:42:31 +0000

    Fix sitemap XML parse error and use toAtomString for lastmod
    - Wrap <?xml ?> declaration in PHP echo to prevent short-tag parsing
    - Use toAtomString() instead of toW3cDateString() (works on Eloquent updated_at)

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,9 +1,9 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($allPages as $path => $lastmod)
     <url>
         <loc>{{ $baseUrl }}{{ $path }}</loc>
-        <lastmod>{{ optional($lastmod)->toW3cDateString() ?? now()->toW3cDateString() }}</lastmod>
+        <lastmod>{{ optional($lastmod)?->toAtomString() ?? now()->toAtomString() }}</lastmod>
         <changefreq>{{ str_starts_with($path, '/i/') ? 'weekly' : 'monthly' }}</changefreq>
         <priority>{{ $path === '/' ? '1.0' : (str_starts_with($path, '/templates/') ? '0.8' : '0.6') }}</priority>
     </url>
